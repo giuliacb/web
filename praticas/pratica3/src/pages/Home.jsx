5ef7e094977a322e6404e7040d0a4b6c7f4590c2
Giulia Campelo <giuliacampelobezerra@gmail.com>
2024-05-07 14:47:59 -0300

    terminando pratica3

--- praticas/pratica3/src/pages/Home.jsx
+++ praticas/pratica3/src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import ContatosContext from "../contexts/ContatosContext";
+
 function Home(){
+    const {meusContatos} = useContext(ContatosContext);
+   
     return (
         <>
             <h2>Meus Contatos</h2>
+            <ul>
+                {meusContatos.map((contato, key) => 
+                    <li id={key}>
+                        {contato.nome} - {contato.telefone}
+                    </li>)}
+            </ul> 
         </>
     );
 }
--- praticas/pratica3/src/pages/Novo.jsx
+++ praticas/pratica3/src/pages/Novo.jsx
@@ -1,7 +1,51 @@
+import { useState, useContext } from "react";
+import ContatosContext from "../contexts/ContatosContext";
+import { useNavigate } from "react-router-dom";
+
 function Novo(){
+    const [nome, setNome] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [erroNome] = useState("");
+    const [erroTelefone] = useState("");
+
+    const incluirContato = useContext(ContatosContext);
+    const navigate = useNavigate();
+
+    const novo = {
+        nome: nome,
+        telefone: telefone
+    };
+
+    function handleSubmit(){
+        preventDefault();
+        incluirContato(novo);
+        navigate('/');
+    }
+
     return (
         <>
             <h2>Novo Contato</h2>
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="nome">Nome:</label>
+                <input
+                    type="text"
+                    id="nome"
+                    value={nome}
+                    onChange={(event) => setNome(event.target.value)}
+                />
+                {erroNome && <p className="erro">{erroNome}</p>}
+
+            <label htmlFor="telefone">Telefone:</label>
+                <input
+                    type="text"
+                    id="telefone"
+                    value={telefone}
+                    onChange={(event) => setTelefone(event.target.value)}
+                />
+                {erroTelefone && <p className="erro">{erroTelefone}</p>}
+            
+            <button type="submit">Enviar</button>
+            </form>
         </>
     );
 }
