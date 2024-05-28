d872f616955953d1923ac676dc6951470e479635
Giulia Campelo <giuliacampelobezerra@gmail.com>
2024-05-28 09:41:12 -0300

    pratica4!

--- praticas/pratica3/src/pages/Editar.jsx
+++ praticas/pratica3/src/pages/Editar.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useEffect, useState, useContext } from 'react';
+import { ContatosContext } from '../contexts';
+import { useParams } from 'react-router-dom';
 
 const Editar = () => {
+    const [nome, setNome] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [erroNome, setErroNome] = useState("");
+    const [erroTelefone, setErroTelefone] = useState("");
+    
+    const {id} = useParams();
+
     return (
         <h2>Editar Contato</h2>
     );
